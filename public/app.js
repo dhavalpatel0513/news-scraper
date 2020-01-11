405d0981fa0cbb154b98f00242b2f53df21313db
Dhaval Patel <dhavalpatel@Dhavals-MacBook-Pro.local>
2020-01-11 10:45:36 -0500

    changes code to fix heroku update problem

--- public/app.js
+++ public/app.js
@@ -1,4 +1,3 @@
-// Grab the articles as a json
 $.getJSON("/articles", function(data) {
   // For each one
   for (var i = 0; i < data.length; i++) {
@@ -8,19 +7,14 @@ $.getJSON("/articles", function(data) {
 });
 
 
-// Whenever someone clicks a p tag
 $(document).on("click", "p", function() {
-  // Empty the notes from the note section
-  $("#notes").empty();
-  // Save the id from the p tag
-  var thisId = $(this).attr("data-id");
 
-  // Now make an ajax call for the Article
+  $("#notes").empty();
+  var thisId = $(this).attr("data-id");
   $.ajax({
     method: "GET",
     url: "/articles/" + thisId
   })
-    // With that done, add the note information to the page
     .then(function(data) {
       console.log(data);
       // The title of the article
@@ -42,31 +36,25 @@ $(document).on("click", "p", function() {
     });
 });
 
-// When you click the savenote button
+
 $(document).on("click", "#savenote", function() {
-  // Grab the id associated with the article from the submit button
+  
   var thisId = $(this).attr("data-id");
 
-  // Run a POST request to change the note, using what's entered in the inputs
   $.ajax({
     method: "POST",
     url: "/articles/" + thisId,
     data: {
-      // Value taken from title input
       title: $("#titleinput").val(),
-      // Value taken from note textarea
       body: $("#bodyinput").val()
     }
   })
-    // With that done
     .then(function(data) {
-      // Log the response
       console.log(data);
-      // Empty the notes section
       $("#notes").empty();
     });
 
-  // Also, remove the values entered in the input and textarea for note entry
+  
   $("#titleinput").val("");
   $("#bodyinput").val("");
 });
